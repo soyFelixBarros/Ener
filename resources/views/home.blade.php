6a4634f32bea0fc4a1678a369b8789a3b344ff27
Felix Barros <soyFelixBarros@gmail.com>
2017-02-07 15:07:32 -0300

    Refactorizacion

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <article>
+                <h3>Noticias</h3>
+            </article>
+        </div>
+    </div><!-- .row -->
+</div><!-- .container -->
+@endsection

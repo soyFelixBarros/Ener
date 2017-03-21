578071c4f89f49a97d9538a8e4baaed83197fba8
Felix Barros <soyFelixBarros@gmail.com>
2017-03-20 21:55:28 -0300

    Ref

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -12,8 +12,8 @@ class HomeController extends Controller
 {
     public function index(Request $request)
     {
-        // $ip = $request->ip();
-        $ip = file_get_contents('https://api.ipify.org');
+        $ip = $request->ip();
+        // $ip = file_get_contents('https://api.ipify.org');
         $query = @unserialize(file_get_contents('http://ip-api.com/php/'.$ip));
 
         if (isset($query) && $query['status'] == 'success') {

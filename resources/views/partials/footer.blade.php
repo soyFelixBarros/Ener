e7f62b381f326ac2ef011f52149bb4579a6d081f
Felix Barros <soyFelixBarros@gmail.com>
2017-06-04 18:16:03 -0300

    Refactorizacion

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,15 +1,15 @@
 <footer class="footer">
 	<div class="container">
 		<div class="row">
-			<div class="col-xs-10">
+{{-- 			<div class="col-sm-10">
 				<ul class="nav nav-pills">
 					<li><a href="{{ route('about') }}">Nosotros</a></li>
 					<li><a href="#">Blog</a></li>
 					<li><a href="#">Api</a></li>
 					<li><a href="#">Contacto</a></li>
 				</ul>
-			</div>
-			<div class="col-xs-2 text-right">
+			</div> --}}
+			<div class="col-sm-12 text-center">
 				<p>&copy; {{ date('Y') }} Cablera Online</p>
 			</div>
 		</div>

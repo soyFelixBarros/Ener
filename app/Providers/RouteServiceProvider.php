718fbedaba19a391765a881eae169314357c9578
Felix Barros <soyFelixBarros@gmail.com>
2017-07-01 14:29:08 -0300

    Refactorizacion

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -24,8 +24,6 @@ class RouteServiceProvider extends ServiceProvider
     public function boot()
     {
         Route::pattern('id', '[0-9]+');
-        Route::pattern('country', '[a-z]+');
-        Route::pattern('province', '[a-z]+');
 
         parent::boot();
 

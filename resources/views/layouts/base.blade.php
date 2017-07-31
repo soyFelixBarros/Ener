30245b1542c662e31b32f00b4c310ea1ea39edcb
Felix Barros <soyFelixBarros@gmail.com>
2017-07-31 09:10:35 -0300

    Refactorizacion

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -1,15 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="row">
-    <div class="col-md-3">
-        @if (Request::is('settings/*'))
-        @include('shared.nav-settings')
-        @endif
-	</div>
-    <div class="col-md-9">
-    @include('shared.status')
-    @yield('content.base')
-    </div>
-</div><!-- .row -->
+<div class="container">
+	<div class="row">
+	    <div class="col-md-3">
+	        @if (Request::is('settings/*'))
+	        @include('shared.nav-settings')
+	        @endif
+		</div>
+	    <div class="col-md-9">
+	    @include('shared.status')
+	    @yield('content.base')
+	    </div>
+	</div><!-- .row -->
+</div>
 @endsection

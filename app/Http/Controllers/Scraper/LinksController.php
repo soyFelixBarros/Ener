fb4b024ce45a01941d7e2fbc8a94cbbec5816b26
Felix Barros <soyFelixBarros@gmail.com>
2018-01-30 10:53:24 -0300

    minimum stability composer

--- app/Http/Controllers/Scraper/LinksController.php
+++ app/Http/Controllers/Scraper/LinksController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Scraper;
 
 use App\Link;
+use Felix\Scraper\Http;
+use Felix\Scraper\Crawler;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -10,42 +12,7 @@ class LinksController extends Controller
 {
     public function __construct()
     {
-        // Tiempo de espera para la carga de los sitios web
-        ini_set('default_socket_timeout', 30);
-    }
-
-    /**
-     * Obtener el status HTTP de una url.
-     * 
-     * @return Integer
-     */
-    private function get_http_response_code($url) {
-        $headers = @get_headers(urldecode($url));
-
-        return (int) substr($headers[0], 9, 3);
-    }
-
-    private function toScrape($settings)
-    {
-        $settings = (array) $settings;
-
-        if (count($settings) > 0) {
-            // Decodifica una cadena cifrada como URL
-            $url = urldecode($settings[0]);
-
-            // Transmite un fichero entero a una cadena
-            $html = @file_get_contents($url);
-
-            // Obtener el status HTTP
-            // $status = (int) substr($http_response_header[0], 9, 3);
-
-            if ($html !== false) {
-                // Buscamos el contenido en el html
-                $crawler = new Crawler($html);
-                return $crawler->evaluate($settings[1]);
-            }
-        }
-        return false;
+        $crawler = new Crawler();
     }
 
     private function hasScheme($path, $schemes = array('http'))
@@ -71,10 +38,10 @@ class LinksController extends Controller
         // Link activo con la fecha de actulización mas antigua.
         $link = Link::where('active', 1)->oldest('updated_at')->first();
 
-        // Obtener el status de la URL
-        $status = $this->get_http_response_code($link->url);
+        $content = $this->crawler->start($link->url, '//');
 
-        if ($status == 200) {
+        if ($content->count()) {
+            dd($content);
             //return 'Enviar link para ser procesado.';
             // event(new \App\Events\PostScraped($post));
         } else {
@@ -85,7 +52,5 @@ class LinksController extends Controller
         $link->updated_at = date('Y-m-d H:i:s');
 
         $link->save();
-
-        dd($link);
 	}
 }

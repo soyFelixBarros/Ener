c27227f56a466e4ebdbf6004aa105d35b2ea8084
Felix Barros <soyFelixBarros@gmail.com>
2018-02-28 13:55:57 -0300

    refactor 4

--- resources/views/shared/posts.blade.php
+++ resources/views/shared/posts.blade.php
@@ -37,24 +37,23 @@
             </a>
             @endif 
             <div class="card-body pt-3">
-                    <p class="card-text font-weight-light mb-0">
-                            <small>
-                                @if (auth()->check())
-                                    @if (auth()->user()->hasRole('admin'))
-                                    <a href="{{ route('admin_posts_edit', ['id' => $post->id]) }}" class="text-info" title="Editar noticia"><i class="far fa-edit"></i></a> /
-                                    @endif
-                                @endif
-        
-                                @if(! request()->is('newspapers/*'))
-                                <a href="{{ route('newspaper_show', ['newspaper' => $post->newspaper->slug]) }}" class="text-dark">{{ $post->newspaper->name }}</a> - 
-                                @endif
-                                <time class="timeago text-muted" datetime="{{ $post->updated_at }}" title="{{ $post->updated_at }}"></time>
-                            </small>
-                        </p>
                 <h5 class="card-title"><a href="{{ $post->url }}" target="_blank" rel="bookmark">{{ $post->title }}</a></h5>
                 @if ($post->content)
                 <p class="card-text">{{ str_limit($post->content, 185) }}</p>
                 @endif
+                <p class="card-text font-weight-light">
+                    <small>
+                        @if (auth()->check())
+                            @if (auth()->user()->hasRole('admin'))
+                            <a href="{{ route('admin_posts_edit', ['id' => $post->id]) }}" class="text-info" title="Editar noticia"><i class="far fa-edit"></i></a> /
+                            @endif
+                        @endif
+                        @if(! request()->is('newspapers/*'))
+                        <a href="{{ route('newspaper_show', ['newspaper' => $post->newspaper->slug]) }}" class="text-dark">{{ $post->newspaper->name }}</a> - 
+                        @endif
+                        <time class="timeago text-muted" datetime="{{ $post->updated_at }}" title="{{ $post->updated_at }}"></time>
+                    </small>
+                </p>
             </div><!-- .card-body -->
         </article><!-- .card -->
         @endforeach

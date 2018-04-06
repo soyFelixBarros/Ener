79a49788842112f9f5d4b03fc97b69212c19a7fb
Felix Barros <soyfelixbarros@gmail.com>
2018-04-06 00:39:15 -0300

    refactor 2

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,7 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-sm-12 col-md-2 mb-3">
+        <div class="col-sm-12 col-md-2 mb-4">
             @include('shared.sidebar-left', ['categories' => $categories])
         </div>
         <div class="col-sm-12 col-md-6">

a0b2823201f44af4bb4e8566842d388fafd83b7b
Felix Barros <soyfelixbarros@gmail.com>
2018-05-15 09:17:27 -0300

    refactor 2

--- resources/views/admin/newspapers/index.blade.php
+++ resources/views/admin/newspapers/index.blade.php
@@ -4,7 +4,7 @@
 <div class="card">
 	<div class="card-header clearfix">
 		<div class="float-left">
-			<h5 class="mt-1 mb-1">{{ $newspapers->count() }} {{ $title }}</h5>
+			<h5 class="mt-1 mb-1">{{ $newspapers->total() }} {{ $title }}</h5>
 		</div>
 		<div class="float-right">
 			<a href="{{ route('admin_newspapers_create') }}" data-toggle="tooltip" data-placement="left" class="btn btn-success btn-sm" role="button" title="Agregar diario"><i class="fas fa-plus"></i></a>	
